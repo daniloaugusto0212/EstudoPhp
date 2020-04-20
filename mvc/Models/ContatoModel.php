c945c74bde1b82ba365957f6d96fa506c0a0bbfe
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-04-19 21:18:15 -0300

    update

--- mvc/Models/ContatoModel.php
+++ mvc/Models/ContatoModel.php
@@ -4,8 +4,8 @@
     class ContatoModel{
 
         public static function enviarFormulario(){
-            $mail = new \Email('smtp.hostinger.com.br','contato@sitedan.com.br','681015','Danilo');
-            $mail->addAdress('contato@sitedan.com.br','DanSol');
+            $mail = new \Email('smtp.hostinger.com.br','contato@dansol.com.br','','Danilo');
+            $mail->addAdress('contato@dansol.com.br','DanSol');
             $mail->formatarEmail(array('assunto'=> 'Mensagem do site','corpo'=>'Aqui é uma mensagem do site'));
             $mail->enviarEmail();
         }
