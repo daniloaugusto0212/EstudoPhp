ce5d9a35b979729499c536f09a33d931ff13186e
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-04-20 14:34:20 -0300

    try e catch

---
+++ conceitosAvancadosPHP/tryCatch/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    class Teste  
+    {
+        public function exemplo($par){
+            if ($par != 10) {
+                throw new Exception("Ocorreu um erro!");                
+            }else{
+                echo 'Sucesso!';
+            }
+        }
+
+    }
+
+    $teste = new Teste();
+    
+
+    try {
+        $teste->exemplo(10);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        
+    }
+
+?>
